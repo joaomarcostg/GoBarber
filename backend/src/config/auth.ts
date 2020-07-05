9f08f816694ac1c1a9347fa3600e1e7c4e152137
João Marcos <joaogardingo@clippingcacd.com.br>
2020-07-05 15:51:52 -0300

    WIP estruturacao do backend

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'd3fcb87eba6c413cda07bed725db1dd3',
+    expiresIn: '1d',
+  }
+}
--- backend/src/database/migrations/1589757581199-CreateAppointments.ts
+++ backend/src/database/migrations/1589757581199-CreateAppointments.ts
@@ -1,14 +1,14 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1588472801861 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {
+export class CreateAppointments1589757581199 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
         name: 'appointments',
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -16,19 +16,27 @@ export class CreateAppointments1588472801861 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
           },
         ],
       }),
     );
   }
 
-  public async down(queryRunner: QueryRunner): Promise<any> {
+  public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropTable('appointments');
   }
 }
